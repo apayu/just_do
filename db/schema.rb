46fcac36bbb2e7455f649cbe7d1ba488fb992386
apa yu <rx836@hotmail.com>
2020-03-13 22:58:11 +0800

    add task finish time

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_13_102828) do
+ActiveRecord::Schema.define(version: 2020_03_13_144738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_03_13_102828) do
     t.string "content", default: "", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "finish_time", null: false
   end
 
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
   factory :task do
     name { Faker::Name.name }
     content  { Faker::Lorem.paragraph }
-    # finish_time { Faker::Date.between(from: 2.days.ago, to: Date.today) }
+    finish_time { Faker::Date.between(from: 2.days.ago, to: Date.today) }
   end
 end
