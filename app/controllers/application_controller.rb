3a0915fe8d17a8284cbeb6530f2f59d28fed2765
apa yu <rx836@hotmail.com>
2020-03-25 12:25:35 +0800

    optimization layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
   helper_method :logged_in?
+  helper_method :admin?
 
   def current_user
     User.find_by(id: session[:user_id])
@@ -8,6 +9,10 @@ class ApplicationController < ActionController::Base
 
   def logged_in?
     !current_user.nil?
+  end
+
+  def admin?
+    current_user.has_role? :admin
   end
 
   def authorized
